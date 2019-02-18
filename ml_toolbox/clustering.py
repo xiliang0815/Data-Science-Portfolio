043808ba5551f2879b86803c5fd7532d313b97c7
Xi Liang <xiliang0815@gmail.com>
2019-02-17 17:12:22 -0800

    updated DBSCAN function

--- ml_toolbox/clustering.py
+++ ml_toolbox/clustering.py
@@ -60,9 +60,44 @@ def calculate_dbscan_eps(X_preprocessed, n_neighbours = 100):
 	eps_value = avg_dist_df[avg_dist_df['clst'] == best_clst]['avg_dist'].values[0]
 	return(eps_value)
 
+def cluster_dbscan(X_preprocessed, dbscan_unique_clst = 10, dbscan_init_clst_num = 2 , dbscan_n_iter = 20):
+	iteration = 0
+	dbscan_perf_df = pd.DataFrame(columns = ['unique_clst', 'avg_silhouette_score', 'n_noise'])
+	optimal_eps = calculate_dbscan_eps(X_preprocessed)
+
+	while dbscan_unique_clst >=3 | iteration < dbscan_n_iter:
+		dbscan_tmp = DBSCAN(eps = optimal_eps, min_samples= dbscan_init_clst_num).fit(X_preprocessed)
+		labels = dbscan_tmp.labels_
+		eval_df = X_preprocessed.copy()
+		eval_df['label'] = labels
+		n_noise = sum(eval_df['label'] == -1)
+		eval_df[eval_df['label']!=-1].reset_index(drop=True, inplace=True)
+
+		unique_clst = eval_df['label'].nunique()
+		avg_silhouette_score = metrics.silhouette_score(eval_df.drop(columns='label'), labels)
+
+		dbscan_perf_df = dbscan_perf_df.append(pd.DataFrame({'unique_clst': [unique_clst],
+														     'avg_silhouette_score': [avg_silhouette_score],
+														     'n_noise': [n_noise]
+															}))
+
+		dbscan_init_clst_num += 1
+		iteration +=1
+
+	dbscan_perf_df.reset_index(drop=True, inplace=True)
+	best_clst_num = dbscan_perf_df.loc[dbscan_perf_df['avg_silhouette_score'].argmax()]['unique_clst']
+	best_score = round(dbscan_perf_df.avg_silhouette_score.max(), 5)
+
+	dbscan_opt = DBSCAN(eps = optimal_eps, min_samples = best_clst_num).fit(X_preprocessed)
+	output_df = X_preprocessed.copy()
+	output_df['label'] = dbscan_opt.labels_
+
+	return(output_df, best_score)
 
 def clustering(X_preprocessed, method='kmeans', on_pca=False, pick_best=False,
-               cluster_min=1, cluster_max=10, step=1):
+               cluster_min=1, cluster_max=10, step=1,
+               dbscan_unique_clst = 10, dbscan_init_clst_num =2, dbscan_n_iter = 20):
+
     if method == 'kmeans':
         if pick_best == False:
             elbow_df = pd.DataFrame(columns=['clst', 'sse'])
@@ -89,7 +124,7 @@ def clustering(X_preprocessed, method='kmeans', on_pca=False, pick_best=False,
             kmeans = KMeans(n_clusters=best_clst, random_state=123).fit(X_preprocessed)
             clst_pred = kmeans.predict(X_preprocessed)
 
-            output_df = X_preprocessed
+            output_df = X_preprocessed.copy()
             output_df['clst'] = clst_pred
             
             print("The best average Silhouette score was {:.3f} at cluster {}.".format(best_score, best_clst))
@@ -98,9 +133,15 @@ def clustering(X_preprocessed, method='kmeans', on_pca=False, pick_best=False,
         elif type(pick_best)==int:
             kmeans = KMeans(n_clusters=pick_best, random_state=123).fit(X_preprocessed)
             clst_pred = kmeans.predict(X_preprocessed)
-            X_preprocessed['clst'] = clst_pred
-            return(X_preprocessed)
+
+            output_df = X_preprocessed.copy()
+            output_df['clst'] = clst_pred
+
+            return(output_df)
         
     elif method == 'dbscan':
     	#determine eps using knn and min_sample using silhouette score
+    	score, output_df = cluster_dbscan(X_preprocessed, dbscan_unique_clst=dbscan_unique_clst, 
+    									  dbscan_init_clst_num=dbscan_init_clst_num , dbscan_n_iter = dbscan_n_iter)
+    	return(score, output_df)
     #elif method == 'gaussian_mixture':
